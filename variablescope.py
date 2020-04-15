a89771903b5fa4ed8834866f3232aa99a2c5a1e9
omarbelkady <31806568+omarbelkady@users.noreply.github.com>
2020-04-15 19:25:02 +0100

    Create variablescope.py

---
+++ variablescope.py
@@ -0,0 +1,11 @@
+a=928
+def corn():
+    print(a)
+
+def fudge():
+    print(a)
+
+corn()
+fudge()
+#only variables inside the function can access it however if a variable is created outside
+#the functions both functions can access it this is called the variable's scope
