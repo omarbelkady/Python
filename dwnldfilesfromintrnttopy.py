7b53f4e92e1e44fbdc26aa0a8628ed12cfde0081
omarbelkady <31806568+omarbelkady@users.noreply.github.com>
2020-04-15 19:43:11 +0100

    Update dwnldfilesfromintrnttopy.py
    How to download images from the internet in Python

--- dwnldfilesfromintrnttopy.py
+++ dwnldfilesfromintrnttopy.py
@@ -4,7 +4,7 @@ goog_url="http://real-chart-finance.yahoo.com/table-csv?s=GOOG&d=8&e=2&f=2014&g=
 
 def download_stock_data(csv_url):
 	response=request.urlopen(csv_url)
-  csv=response.read()#this will read all the data from the url you pointed it to
+  	csv=response.read()#this will read all the data from the url you pointed it to
 	csv_str=str(noname)#this will convert the the no name variable we read to a string
 	lines=csv_str.split('\\n')
 	dest_url()= r'goog.csv'#this is a raw string make a file
