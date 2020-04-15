681fe29016a70e1a83849e8b91afa5b53d362f4c
omarbelkady <31806568+omarbelkady@users.noreply.github.com>
2020-04-15 19:25:42 +0100

    Create keywordarguments.py

---
+++ keywordarguments.py
@@ -0,0 +1,6 @@
+def dumb_sentence(name="Omar", action='ate', item="salmon"):
+    print(name,action, item)
+print(dumb_sentence)
+dumb_sentence("sally", 'farts', 'gently')
+dumb_sentence(action='flew', item='a plane', name='Rick')
+dumb_sentence(action='is',item='so cool')
