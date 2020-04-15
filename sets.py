12f69ef904df2b42214f7494027aefb102851d32
omarbelkady <31806568+omarbelkady@users.noreply.github.com>
2020-04-15 19:27:59 +0100

    Create sets.py

---
+++ sets.py
@@ -0,0 +1,8 @@
+#sets instead of lists they cannot have duplicates and use curly braces
+
+groceries={'cereal', 'milk', 'starcrunches', 'beer', 'duck tape','lotion','beer'}
+print(groceries)
+if 'milk' in groceries:
+    print("you are already have milk")
+else:
+    print("oh yes you do need milk")
