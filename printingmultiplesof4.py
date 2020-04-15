9f0e031f31fe2f34bda83dfa069cb4fa581c21f7
omarbelkady <31806568+omarbelkady@users.noreply.github.com>
2020-04-15 19:20:45 +0100

    Create printingmultiplesof4.py

---
+++ printingmultiplesof4.py
@@ -0,0 +1,12 @@
+magicnumber=62
+for n in range(101):
+	if n is magicnumber:
+		print(n, "is the magic number")
+		break
+	else:
+		print(n)
+for n in range(4,39,4):
+	if n%4 is 0:
+	    print(n)
+	else:
+		print("number is not divisible by 4")
