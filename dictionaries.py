a5513e792756876b8356cd7c4f1313102645e901
omarbelkady <31806568+omarbelkady@users.noreply.github.com>
2020-04-15 19:30:40 +0100

    Create dictionaries.py

---
+++ dictionaries.py
@@ -0,0 +1,5 @@
+classmates={'tony': ' cool but smells', 'anthony': ' pro-gamer','emma': ' sits behind me'}
+print(classmates['emma'])
+#Loop through the keys
+for k,v in classmates.items():
+    print(k+v)
