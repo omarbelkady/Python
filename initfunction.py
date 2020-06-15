2a3f0a816723f1e7b5b16c1d680550a773453d68
omarbelkady <31806568+omarbelkady@users.noreply.github.com>
2020-06-15 20:33:04 +0100

    Update initfunction.py

--- initfunction.py
+++ initfunction.py
@@ -1,8 +1,8 @@
 class Tuna:
-		def __init__(self):
-			print("djlfajldjfla")
-		def swim(self):
-			print("I am swimming")
+	def __init__(self):
+		print("djlfajldjfla")
+	def swim(self):
+		print("I am swimming")
 flipper=Tuna()#overloading
 flipper.swim()#creating an instance of the class
 
