4f8a1ce07d84da2c4ca67733c9e10da7762dfab8
omarbelkady <31806568+omarbelkady@users.noreply.github.com>
2020-06-15 20:24:45 +0100

    Update crawlentireinternet.py

--- crawlentireinternet.py
+++ crawlentireinternet.py
@@ -4,14 +4,14 @@ from bs4 import BeautifulSoup #this package imports all the data from the websit
 def trade_spider(max_pages):
 	page=1
 	while page < max_pages:
-		url= “https://buckyroom.org/trade/search.php?page=”+str(page)
+		url= "https://buckyroom.org/trade/search.php?page="+str(page)
 		source_code=request.get(url)
 		#all the page source is now stored in the source_code variable
 		plain_text=source_code.text
 		soup=BeautifulSoup(plain_text)#creating a beautiful soup object
 		#go to page source to find all the links for specific articles to browse in a class
 		for link in soup.findAll(‘a’,{‘class’: ‘item-name’}): #getting all the titles
-			href= “http://buckyroom.org/”+link.get(href)#we want only the data in the href
+			href= "http://buckyroom.org/"+link.get(href)#we want only the data in the href
 				title=link.string()
 				#print(href)
         #print(title)
@@ -21,12 +21,12 @@ def get_single_item_data(item_url):
 	source_code=request.get(item_url)
 	plain_text=source_code.text
 	soup=BeautifulSoup(plain_text)
-	#for item_name in soup.findAll(‘a’, {‘class’: ‘item-name’}):
-	for item_name in soup.findAll(‘div’, {‘class’: ‘i-name’}):
+	#for item_name in soup.findAll('a', {‘class’: 'item-name'}):
+	for item_name in soup.findAll('div', {‘class’: 'i-name'}):
 print(item-name.string)
 #how to crawl a page that is a link on the page you already crawled
-for link in soup.findAll(‘a’):
-	href= “http://www.buckysroom.org” + link.get(‘href’)
+for link in soup.findAll('a'):
+	href= "http://www.buckysroom.org" + link.get('href')
 print(href)
 	
 	
