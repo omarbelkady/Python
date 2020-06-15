4e114356a7ce504a54b5a4e9f7ee2004514f01e7
omarbelkady <31806568+omarbelkady@users.noreply.github.com>
2020-06-15 13:01:15 +0100

    Update tryexceptfinally.py

--- tryexceptfinally.py
+++ tryexceptfinally.py
@@ -1,13 +1,13 @@
 while True:
 	try:
-		number=int(input(“What’s your favorite number dude?\n”))
+		number=int(input("What’s your favorite number dude?\n"))
 		print(18/number)
 		break
 	except ValueError:
-		print(“Make sure to enter only digits”)
+		print("Make sure to enter only digits")
 	except ZeroDivisionError:
-		print(“You cannot divide by Zero”)
+		print("You cannot divide by Zero")
 	except:
 		break
 	finally:				#finally keyword no matter what happens this will always be executed
-		print(“loop complete”)
+		print("loop complete")
