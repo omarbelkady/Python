9da3e560cb643d0838c5c16cacc44163503d8acc
omarbelkady <31806568+omarbelkady@users.noreply.github.com>
2020-06-15 20:25:48 +0100

    Update crawlentireinternet.py

--- crawlentireinternet.py
+++ crawlentireinternet.py
@@ -21,8 +21,8 @@ def get_single_item_data(item_url):
 	source_code=request.get(item_url)
 	plain_text=source_code.text
 	soup=BeautifulSoup(plain_text)
-	#for item_name in soup.findAll('a', {‘class’: 'item-name'}):
-	for item_name in soup.findAll('div', {‘class’: 'i-name'}):
+	#for item_name in soup.findAll('a', {'class': 'item-name'}):
+	for item_name in soup.findAll('div', {'class': 'i-name'}):
 print(item-name.string)
 #how to crawl a page that is a link on the page you already crawled
 for link in soup.findAll('a'):
