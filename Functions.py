60f43e5fee26e109435fca0b55e6a457c91629c9
omarbelkady <31806568+omarbelkady@users.noreply.github.com>
2020-06-24 22:57:17 +0100

    Update Functions.py

--- Functions.py
+++ Functions.py
@@ -1,15 +1,14 @@
 #the def keyword means you are creating or making a function
 def beef():
-  print("daym functions are cool")
+	print("daym functions are cool")
 
 #i call the beef function
 beef()
 
 def bitcoin_to_usd(btc):
-  amount=btc*527
-  print(amount)
+	amount=btc*527
+	print(amount)
     
 #i call the beef function for a second time
 beef()
 bitcoin_to_usd(3.85)
-
