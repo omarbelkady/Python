3b12b34d25feff82543faf1dca85018497d69b1c
omarbelkady <31806568+omarbelkady@users.noreply.github.com>
2020-06-24 22:59:55 +0100

    Create Function1.py

---
+++ FunctionsAreTheBest/Function1.py
@@ -0,0 +1,9 @@
+#def goPresentYourself():
+#	print("Hi my name is Omar I love STEM and hate reading")
+#
+#So I run the program and nothing gets printed out to me.
+#This is because I have not called/invoked the function
+def goPresentYourself():
+	print("Hi my name is Omar I love STEM and hate reading")
+
+goPresentYourself()
