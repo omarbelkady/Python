d41cf148db1a76ea9d507593adbda551678747da
omarbelkady <31806568+omarbelkady@users.noreply.github.com>
2020-06-24 21:26:23 +0100

    Update and rename Dictionaries.py to DictsNdListsDictionaries.py

--- Dictionaries.py
+++
@@ -1,5 +0,0 @@
-classmates={'tony': ' cool but smells', 'anthony': ' pro-gamer','emma': ' sits behind me'}
-print(classmates['emma'])
-#Loop through the keys
-for k,v in classmates.items():
-    print(k+v)
---
+++ DictsNdListsDictionaries.py
@@ -0,0 +1,5 @@
+classmates={'alan': ' C LOVER', 'Noor': ' hottest-CS','Laura': ' dumb B**** who <3 Tru** THE IDIOT'}
+print(classmates['Noor'])
+#Loop through the keys
+for k,v in classmates.items():
+    print(k+v)
