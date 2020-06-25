53b6f26700a7ae375c53acbb751c140cedfc0b5b
omarbelkady <31806568+omarbelkady@users.noreply.github.com>
2020-06-25 08:53:59 +0100

    Update Range.py

--- Range.py
+++ Range.py
@@ -1,7 +1,9 @@
 for x in range (10):
   print (x)
 
+#This finishes at 9 then it will hop into the next functions and print 11,13,15... all the way to 29
 for x in range(5,30,2):
   print(x)
-#for x in range(start,stop,step):
-#	print(x)
+
+#A RANGE FUNCTION WORKS AS SUCH
+#range(start,stop,step):
