bb68fa8265924d0a4f6a0b0875574add0e139dce
omarbelkady <31806568+omarbelkady@users.noreply.github.com>
2020-06-26 14:44:33 +0100

    Create ForLoopIfFoundBreak.py

---
+++ Loops/ForLoopIfFoundBreak.py
@@ -0,0 +1,6 @@
+good_majors=["CS","Computer Engineering", "Literature" ,"Software Engineering", "Electrical Engineering"]
+
+for o in good_majors:
+    print(o)
+    if o == "Literature":
+        break
