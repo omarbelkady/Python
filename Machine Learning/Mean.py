560e57d3e58e0fad9b6f2706f55846ad40b255e1
omarbelkady <31806568+omarbelkady@users.noreply.github.com>
2020-06-26 19:23:55 +0100

    Create Mean.py

---
+++ Machine Learning/Mean.py
@@ -0,0 +1,8 @@
+import numpy
+
+studentNums=[193,419,419,391,193,192,193,192,995,943,381,573,371]
+
+x=numpy.mean(studentNums)
+
+#Format the average of the list to 4 decimal points
+print("The average is: {0:.4f}".format(x))
