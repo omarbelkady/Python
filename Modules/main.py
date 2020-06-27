f01813957eda281d844a7a6ac3292abf15a6a4c1
omarbelkady <31806568+omarbelkady@users.noreply.github.com>
2020-06-27 13:06:37 +0100

    Create main.py

---
+++ Modules/main.py
@@ -0,0 +1,14 @@
+from omarsModule import Reptile
+from Car import *
+#from a specpackage.specmodule import Class
+#calling on the constructor
+gecko = Reptile()
+gecko.userName= "Dingo"
+gecko.sayHi()
+
+
+omarsCar=Car()
+omarsCar.speedLimit(95)
+
+myTrain=Train()
+myTrain.speedLimit(35)
