ca303c41794ee3c9821e1a380eae8d8ee71347d3
omarbelkady <31806568+omarbelkady@users.noreply.github.com>
2020-06-27 13:07:48 +0100

    Create Car.py

---
+++ Modules/Car.py
@@ -0,0 +1,8 @@
+class Car(object):
+
+	def speedLimit(self, speed):
+		print("I am travelling at %d" % speed)
+
+class Train(object):
+    def speedLimit(self,speed):
+        print("Going at bullet speed at this pace: %l" % speed)
