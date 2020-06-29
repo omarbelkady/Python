9ca88e2f8b2187bd5d9ca5173846c627a1cec093
omarbelkady <31806568+omarbelkady@users.noreply.github.com>
2020-06-30 00:36:47 +0100

    Create LoopingThroughDict.py

---
+++ Loops/LoopingThroughDict.py
@@ -0,0 +1,4 @@
+activitiesToNotInvestIn={"COMP":"Terrible","NOLIFE":"FLASK/JAVA","C OR DJANGO OR JAVA":"DJANGO RULES","reading":"NOT MEANT FOR CS",}
+for keys,values in activitiesToNotInvestIn.items():
+    print(keys)
+    print(values)
