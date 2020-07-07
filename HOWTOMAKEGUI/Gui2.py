25d172457c1a973ec3f9e7fdcea417298cdc5842
omarbelkady <31806568+omarbelkady@users.noreply.github.com>
2020-07-07 16:56:26 +0100

    Create Gui2.py

---
+++ HOWTOMAKEGUI/Gui2.py
@@ -0,0 +1,13 @@
+from tkinter import *
+root= Tk()
+
+
+#Creating a label
+omarsLabel=Label(root, text= "Hello There Everyone!!!")
+
+
+#Placing it in the screen
+omarsLabel.pack()
+
+
+root.mainloop()
