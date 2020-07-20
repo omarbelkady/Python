c65165dd67d8cd72310dc0754b84c968d5d2e38e
omarbelkady <31806568+omarbelkady@users.noreply.github.com>
2020-07-20 13:59:27 +0100

    Update matrix_calc.py

--- Machine Learning/matrix_calc.py
+++ Machine Learning/matrix_calc.py
@@ -2,7 +2,12 @@ import numpy as np
 
 o=np.array([1,2,3])
 b=np.array([4,5,6])
+
+#Dot Protect: scaler magnitude but no direction
 print(np.dot(o,b))
+
+#Cross Protect: vector meaning magnitude+direction
+print(np.cross(o,b))
 
 
 #matrix calculation row*colum
@@ -10,7 +15,7 @@ m=np.array([[3,4],[6,9]])
 j=np.array([[2,5],[7,19]])
 
 
-#calculating the determinant
+#calculating the determinant of a 2x2 matrix * 2x2 matrix
 print(np.linalg.det(j))#(2*19)-(5*7)=38-35=3
 
 mu=17
