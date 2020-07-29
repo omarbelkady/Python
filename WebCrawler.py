e95b7ef7f9299077277a223c49d61d1174216797
Omar <omar.belkady@g.austincc.edu>
2020-07-29 23:28:10 +0100

    Fixed The Web Scraping programs

--- WebCrawler.py
+++ WebCrawler.py
@@ -1,12 +1,4 @@
-import requests
-from bs4 import BeautifulSoup
-page = requests.get('https://web.archive.org/web/20121007172955/https://www.nga.gov/collection/anZ1.htm')
-soup = BeautifulSoup(page.text, 'html.parser')
+from urllib.request import urlopen
 
-
-artist_name_list = soup.find(class_='BodyText')
-
-# Pull text from all instances of <a> tag within BodyText div
-artist_name_list_items = artist_name_list.find_all('a')
-for artist_name in artist_name_list_items:
-    print(artist_name.prettify())
+html=urlopen('http://pythonscraping.com/pages/page1.html')
+print(html.read())
