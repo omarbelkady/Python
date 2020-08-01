3360536f57c3ccc2706fad414f60c86e12e41ecb
Omar <omar.belkady@g.austincc.edu>
2020-08-01 20:57:01 +0100

    For Troops_Hassar

---
+++ User Input/troops.py
@@ -0,0 +1,22 @@
+# Hello Mr Troops this program is for you
+
+#This line of code basically prompts you to enter your name and then stores it in a variable
+name = input("What is your name: ")
+
+#Data Types in Python
+
+#str which is a string e.g. "Troops"
+
+#int which is an integer e.g. 5
+
+#float  which is a number with a floating point  e.g. '45.41112'
+
+#complex which is written in standard form a+bi e.g. 6+4j
+
+#list which is an array that holds value and can be changed and you can store any type of data in it  e.g. [1,2,3,4,"bla",5.45, 'a', true]
+
+#tuples which is similar to an array has values stored but they cannot be changed e.g. (6,7,8,9,1,2,3)
+
+#dictionary is a data type which stores keys and a value. I will use a dictionary to map majors by abbreviations e.g. {"cs":"Computer Science", "otto":"Othman", "ce": "Computer Engineering", "apt": "Apartment", "dept":"Department"}
+
+print("So you're name is "+name)
