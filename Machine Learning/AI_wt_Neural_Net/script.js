c6b1d868f5d817764ff41d39ccd55f8144ff1a82
Omar <omar.belkady@g.austincc.edu>
2020-08-04 17:58:41 +0100

    Changed the script of the AI NN

--- Machine Learning/AI_wt_Neural_Net/script.js
+++ Machine Learning/AI_wt_Neural_Net/script.js
@@ -1,5 +1,8 @@
-//How to start Creating an object
-const myNet = new brain.NeuralNetwork()
+//How to start Creating an object and we are going to have 3 different hidden 
+//layers one with 4 nodes, one with 5 nodes and one with 6 nodes
+const myNet = new brain.NeuralNetwork({
+	hiddenLayers:[4,5,6]
+})
 const diagram = document.getElementById('my-diagram')
 
 //give our NN information
@@ -33,3 +36,6 @@ myNet.train([
 
 //Setting our inner HTML to this
 diagram.innerHTML=brain.utilities.toSVG(myNet)
+
+//Test our AI to see if it learned
+console.log(myNet.run([0,0]))
