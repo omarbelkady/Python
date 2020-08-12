3bf5f9cf15e2f9e62d82f1ccc930340d683100a5
Omar <omar.belkady@g.austincc.edu>
2020-08-12 22:29:13 +0100

    How to Create A Class Method in Python

---
+++ Classes/ClassMeths.py
@@ -0,0 +1,14 @@
+class TakeInAndPrintString(object):
+    #init means create params and __init__(self) is the def constructor
+    def __init__(self):
+        self.n = ""
+
+    def returnTheString(self):
+        self.n= input()
+
+    def outputStringToConsoleInUpperCase(self)
+        print(self.n.upper())
+
+classObj = TakeInAndPrintString()
+classObj.returnTheString()
+classObj.outputStringToConsoleInUpperCase()
