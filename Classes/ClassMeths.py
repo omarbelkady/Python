38da5915b7f045c063d854324ea4a973379c7a70
Omar <omar.belkady@g.austincc.edu>
2020-08-12 22:46:13 +0100

    How To Make Class Methods OH MAN THIS IS FUNNY!

--- Classes/ClassMeths.py
+++ Classes/ClassMeths.py
@@ -1,14 +1,16 @@
 class TakeInAndPrintString(object):
     #init means create params and __init__(self) is the def constructor
     def __init__(self):
-        self.n = ""
+        self.l ="AND TAYLOR SWIFT"
 
+    @staticmethod
     def returnTheString(self):
-        self.n= input()
+        #How to print on the same line
+        print(self,end=" ")
 
-    def outputStringToConsoleInUpperCase(self)
-        print(self.n.upper())
+    def outputStringToConsoleInUpperCase(self):
+        print(self.l.upper())
 
 classObj = TakeInAndPrintString()
-classObj.returnTheString()
+classObj.returnTheString("2526_56837_2")
 classObj.outputStringToConsoleInUpperCase()
