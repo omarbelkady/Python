cad160b36a18815720ad9ee9683858651ea17e21
Omar <omar.belkady@g.austincc.edu>
2020-08-18 04:36:58 +0100

    Angela does This to Me All The Time

---
+++ Functions/Death_Or_Not.py
@@ -0,0 +1,8 @@
+def trauma(n):
+    if(n == "book"):
+        print("You will die no joke and it is Angela's fault")
+    else:
+        print("you will live")
+trauma("book")
+print("\n")
+trauma("none")
