36e1df1f725e5be0f29752a27cbda45339c770c7
Omar <omar.belkady@g.austincc.edu>
2020-08-25 20:02:35 +0100

    How To Remove a recurring item in a list at once hAHAHAHA

---
+++ Lists&Dictionaries/OccurOfItemInList.py
@@ -0,0 +1,9 @@
+coolItems= ["Java", "C", "Pintos", "Java" ,"569 53835", "Machine Language", "Compilers", "C-Family PL", "2526 56837 2", "2526 56837 2", "2526 56837 2"]
+
+#coolItems.remove("Java")
+
+while coolItems.count("Java") > 0:
+    coolItems.remove("Java")
+
+print(coolItems)
+print("HAHAHAAAHA 2 56837!")
