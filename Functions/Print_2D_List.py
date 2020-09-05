fa4e83cd317ff0343cc8c0844951b65ceb45662f
Omar <omar.belkady@g.austincc.edu>
2020-09-05 23:34:02 +0100

    Print 2D List

---
+++ Functions/Print_2D_List.py
@@ -0,0 +1,9 @@
+funny_words=[["6342-56837", "2-56837", "746867 56837"],[ "569 53835 56837"],["2 56837","6224463 52648243", "27736259"]]
+#              NEH           2          PINT              LOW                                  MACHINE      ASSEM
+def print_my_2d_list(funny_words):
+    for inside in funny_words:
+        for phrase in inside:
+            print(phrase, end=" ")
+        print()
+print_my_2d_list(funny_words)
+print("Remember 2526 56837 byte code of Compiled Languages not interpreted code")
