ba5cf6316775eb1b7cad1f3e95832c43fcb18bd4
Omar Belkady <31806568+omarbelkady@users.noreply.github.com>
2020-09-17 23:39:51 +0100

    Delete CheckAProperty.py

--- Classes/CheckAProperty.py
+++
@@ -1,9 +0,0 @@
-class IsACLover():
-	#This is my constructor
-	def __init__(self, name,title):
-		self.name = name
-		self.title = title
-
-nelan = IsACLover("Nelan", "CDeveloper")
-if(nelan.title=="CDeveloper"):
-    print("Nelan The Best Person To help you in C And Assembly")
