9baf63417b5a77094bbabd411b40f144254a01a4
Omar <omar.belkady@g.austincc.edu>
2020-09-17 23:38:50 +0100

    Builds and Runs LOL!

---
+++ Classes/CheckAProperty.py
@@ -0,0 +1,9 @@
+class IsACLover():
+	#This is my constructor
+	def __init__(self, name,title):
+		self.name = name
+		self.title = title
+
+nelan = IsACLover("Nelan", "CDeveloper")
+if(nelan.title=="CDeveloper"):
+    print("Nelan The Best Person To help you in C And Assembly")
